0f79939c68c360f435d342e3bb055202826cca8c
JoshHambright <josh.hambright@gmail.com>
2020-12-09 18:32:38 -0500

    Controller Read methods

---
+++ ElevenNote.WEBMVC/Controllers/CategoryController.cs
@@ -0,0 +1,36 @@
+﻿using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.WEBMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private CategoryService CreateCategoryService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new CategoryService(userId);
+            return service;
+        }
+        // GET: Category
+        public ActionResult Index()
+        {
+            var service = CreateCategoryService();
+            var model = service.GetCategories();
+            return View(model);
+        }
+
+        //Get: Category/Details/{id}
+        public ActionResult Details(int id)
+        {
+            var svc = CreateCategoryService();
+            var model = svc.GetCategoryByID(id);
+
+            return View(model);
+        }
+    }
+}
