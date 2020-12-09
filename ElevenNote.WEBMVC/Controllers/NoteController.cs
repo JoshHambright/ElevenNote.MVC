3baf2227c9292925b2fbe35a49c7bd4af34e76c8
JoshHambright <josh.hambright@gmail.com>
2020-12-09 08:43:32 -0500

    Note Models, Note Controller, Note Data Class, Note Index View

---
+++ ElevenNote.WEBMVC/Controllers/NoteController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ElevenNote.Models.NoteModels;
+
+namespace ElevenNote.WEBMVC.Controllers
+{
+    [Authorize]
+    public class NoteController : Controller
+    {
+        // GET: Note
+        public ActionResult Index()
+        {
+            var model = new NoteListItem[0];
+            return View();
+        }
+    }
+}
