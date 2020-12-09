552274ec0850d3c2362fa1e73000091227a5dee1
JoshHambright <josh.hambright@gmail.com>
2020-12-09 07:48:06 -0500

    Add project files.

---
+++ ElevenNote.WEBMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ElevenNote.WEBMVC.Startup))]
+namespace ElevenNote.WEBMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
